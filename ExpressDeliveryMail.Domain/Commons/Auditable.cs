99d56064cfece3f4b9167c885b6901941270525d
Komiljon <komiljonrahmatov03@gmail.com>
2024-03-06 20:52:58 +0500

    Added Branch Entity

--- ExpressDeliveryMail.Domain/Commons/Auditable.cs
+++ ExpressDeliveryMail.Domain/Commons/Auditable.cs
@@ -1,7 +1,10 @@
-﻿namespace ExpressDeliveryMail.Domain.Commons
-{
-    public class Auditable
-    {
+﻿namespace ExpressDeliveryMail.Domain.Commons;
 
-    }
+public abstract class Auditable
+{
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime DeletedAt { get; set; }
+    public bool IsDeleted { get; set; }
 }
