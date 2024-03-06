e224b6da9a321fa1e7b17dd650bfa1e7166abf0d
Komiljon <komiljonrahmatov03@gmail.com>
2024-03-06 21:55:56 +0500

    Added Interfaces

---
+++ ExpressDeliveryMail.Service/Interfaces/IExpressService.cs
@@ -0,0 +1,12 @@
+﻿using ExpressDeliveryMail.Domain.Entities;
+
+namespace ExpressDeliveryMail.Service.Interfaces;
+
+public interface IExpressService
+{
+    ValueTask<Express> CreatedAsync(Express express);
+    ValueTask<Express> UpdateAsync(long id, Express express);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<Express> GetByIdAsync(long id);
+    ValueTask<IEnumerable<Express>> GetAllAsync();
+}
---
+++ ExpressDeliveryMail.Service/Interfaces/ITransportService.cs
@@ -0,0 +1,12 @@
+﻿using ExpressDeliveryMail.Domain.Entities;
+
+namespace ExpressDeliveryMail.Service.Interfaces;
+
+public interface ITransportService
+{
+    ValueTask<Transport> CreatedAsync(Transport transport);
+    ValueTask<Transport> UpdateAsync(long id, Transport transport);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<Transport> GetByIdAsync(long id);
+    ValueTask<IEnumerable<Transport>> GetAllAsync();
+}
