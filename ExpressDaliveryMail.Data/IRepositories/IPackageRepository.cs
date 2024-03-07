25db2d823514dfd4f7175001911f1e3a051e1246
Komiljon <komiljonrahmatov03@gmail.com>
2024-03-07 14:40:17 +0500

    Added Package Service

---
+++ ExpressDaliveryMail.Data/IRepositories/IPackageRepository.cs
@@ -0,0 +1,12 @@
+﻿using ExpressDeliveryMail.Domain.Entities;
+using ExpressDeliveryMail.Domain.Entities.Transports;
+
+namespace ExpressDaliveryMail.Data.IRepositories;
+
+public interface IPackageRepository
+{
+    ValueTask<Package> InsertAsync(Package package);
+    ValueTask<Package> UpdateAsync(long id, Package package);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<IEnumerable<Package>> GetAllAsync();
+}
---
+++ ExpressDeliveryMail.Domain/Entities/PackageUpdateModel.cs
@@ -0,0 +1,20 @@
+﻿using ExpressDeliveryMail.Domain.Entities.Branches;
+using ExpressDeliveryMail.Domain.Entities.Users;
+using ExpressDeliveryMail.Domain.Enums;
+
+namespace ExpressDeliveryMail.Domain.Entities;
+
+public class PackageUpdateModel
+{
+    public long UserId { get; set; }
+    public User User { get; set; }
+    public long StartBranchId { get; set; }
+    public Branch StartBranch { get; set; }
+    public long EndBranchId { get; set; }
+    public Branch EndBranch { get; set; }
+    public PackageStatus Status { get; set; }
+    public PackageCategory Category { get; set; }
+    public float Weight { get; set; }
+    public string ReceiverName { get; set; }
+    public string ReceiverPhone { get; set; }
+}
